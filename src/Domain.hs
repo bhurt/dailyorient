78730620b8cc6d948164348d2edbff9e201735bc
Brian Hurt <bhurt42@gmail.com>
2024-01-01 13:07:20 -0600

    Improvements to defaultLayout.

--- src/Domain.hs
+++ src/Domain.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 module Domain (
     Domain(..),
@@ -11,6 +12,7 @@ module Domain (
     Route(..)
 ) where
 
+    import           Data.Text           (Text)
     import           Server.Yesod.Routes
     import           Yesod
 
@@ -19,7 +21,37 @@ module Domain (
     mkYesodData domainName routes
 
     -- Note: this has to be *after* mkYesodData or it won't compile.
-    instance Yesod Domain
+    instance Yesod Domain where
+
+        makeSessionBackend _ = pure Nothing
+
+        yesodMiddleware = id
+
+        defaultLayout w = do
+            p <- widgetToPageContent (setTitle "The Daily Orientation" <> w)
+            msgs <- getMessages
+            let description :: Text
+                description =
+                    case pageDescription p of
+                        Nothing -> "The Daily Orientation"
+                        Just x  -> x
+            withUrlRenderer [hamlet|
+                $newline never
+                $doctype 5
+                <html>
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport"
+                            content="width=device-width, initial-scale=1.0">
+                        <meta name="author" content="Brian Hurt">
+                        <title>#{pageTitle p}
+                        <meta name="description" content="#{description}">
+                        ^{pageHead p}
+                    <body>
+                        $forall (status, msg) <- msgs
+                            <p class="message #{status}">#{msg}
+                        ^{pageBody p}
+            |]
 
     makeDomain :: IO Domain
     makeDomain = pure Domain
